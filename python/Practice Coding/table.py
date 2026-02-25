8ad676e1bc6654f8d96c9d3e9465b009fb75fc65
pavanmagadum <pavanmagadum8@gmail.com>
2026-02-25 22:27:16 +0530

    Build an logic for an table

---
+++ python/Practice Coding/table.py
@@ -0,0 +1,7 @@
+def Print_Table(number):
+    index = 1
+    while index < 11:
+        result = number * index
+        print(f"{number} * {index} = {result}")
+        index = index+1
+Print_Table(14)
